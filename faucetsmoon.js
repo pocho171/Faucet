fe6277fc7d42df578797715c472ee9cc74f399bf
pocho171 <32076706+pocho171@users.noreply.github.com>
2020-11-26 12:49:52 -0400

    Update faucetsmoon.js

--- faucetsmoon.js
+++ faucetsmoon.js
@@ -6,7 +6,7 @@ var s = [
   "https://bitfun.co/?ref=7DEBD8CC41F9",
   "https://moondoge.co.in/?ref=457791978D9C",
   "https://moonliteco.in/?ref=203E35AD4A1C",
-  "https://moonbitcoin.cash/?ref=9FAA73AF2154",
+  "https://moonbitcoin.cash/?ref=9FAA73AF2154"
   ];
   
 
