99b90a1108d627c42a63b3b71e2d7264fb81e28c
pocho171 <32076706+pocho171@users.noreply.github.com>
2020-11-26 11:00:21 -0400

    Add files via upload

---
+++ faucetsmoon.js
@@ -0,0 +1,13 @@
+var s = [
+  "http://bonusbitcoin.co/?ref=E6DA8A6FFE60",
+  "http://moonbitcoin.cash/?ref=9FAA73AF2154",
+  "http://moondash.co.in/?ref=C9D9211EDB5C",
+  "http://moonbit.co.in/?ref=57E802C0D14C",
+  "http://bitfun.co/?ref=7DEBD8CC41F9",
+  "http://moondoge.co.in/?ref=457791978D9C",
+  "http://moonliteco.in/?ref=203E35AD4A1C",
+  "http://moonbitcoin.cash/?ref=9FAA73AF2154",
+  
+];
+  
+
