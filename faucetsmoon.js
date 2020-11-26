e4addee2974d4a36eb98400ad2a6e3f752678566
pocho171 <32076706+pocho171@users.noreply.github.com>
2020-11-26 12:32:26 -0400

    Update faucetsmoon.js

--- faucetsmoon.js
+++ faucetsmoon.js
@@ -1,13 +1,12 @@
 var s = [
-  "http://bonusbitcoin.co/?ref=E6DA8A6FFE60",
-  "http://moonbitcoin.cash/?ref=9FAA73AF2154",
-  "http://moondash.co.in/?ref=C9D9211EDB5C",
-  "http://moonbit.co.in/?ref=57E802C0D14C",
-  "http://bitfun.co/?ref=7DEBD8CC41F9",
-  "http://moondoge.co.in/?ref=457791978D9C",
-  "http://moonliteco.in/?ref=203E35AD4A1C",
-  "http://moonbitcoin.cash/?ref=9FAA73AF2154",
-  
-];
+  "https://bonusbitcoin.co/?ref=E6DA8A6FFE60",
+  "https://moonbitcoin.cash/?ref=9FAA73AF2154",
+  "https://moondash.co.in/?ref=C9D9211EDB5C",
+  "https://moonbit.co.in/?ref=57E802C0D14C",
+  "https://bitfun.co/?ref=7DEBD8CC41F9",
+  "https://moondoge.co.in/?ref=457791978D9C",
+  "https://moonliteco.in/?ref=203E35AD4A1C",
+  "https://moonbitcoin.cash/?ref=9FAA73AF2154",
+  ];
   
 
